36a42291d6a8aeee3ee112d48e3b67fd99ea96e1
darshansjain <jaindarshan2620@gmail.com>
2024-02-13 14:52:17 +0530

    Changes

--- basic_instagram/android/app/src/main/kotlin/com/example/basic_instagram/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.basic_instagram
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- various_num_count/android/app/src/main/kotlin/com/example/testdemo2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.testdemo2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
